84e2728fa03722817ac039aa7bac9ddeb8bbe432
yashzala <yash.zala@techverito.com>
2023-12-18 20:50:08 +0530

    Created static version

---
+++ src/Product/FilterableProductTable.js
@@ -0,0 +1,72 @@
+import './Style.css'
+
+function ProductCategoryRow({category}) {
+    return <tr>
+        <th align={"left"}>
+            {category}
+        </th>
+    </tr>
+}
+
+
+function ProductRow({product}) {
+    console.log(product.name)
+    return <>
+        <tr>
+            <td>
+                {product.name}
+            </td>
+            <td>
+                {product.price}
+            </td>
+        </tr>
+
+    </>
+}
+
+ProductRow.propTypes = {};
+
+function ProductTable({products}) {
+
+    const categories = new Set()
+    // eslint-disable-next-line array-callback-return
+    products.map(it => {categories.add(it.category)})
+
+    return <>
+        <tr>
+            <th>Name</th>
+            <th>Price</th>
+        </tr>
+        {
+            Array.from(categories).map((category) =>
+                <>
+                    <ProductCategoryRow category={category}></ProductCategoryRow>
+                    {
+                        products.map(product =>
+                            (product.category === category ? <ProductRow product={product}></ProductRow> : null)
+                        )
+                    }
+                </>
+            )
+        }
+    </>;
+}
+
+function SearchBar() {
+    return <>
+        <div><input placeholder={'search'}/></div>
+        <div><input type={"checkbox"}/>Only show product in stock</div>
+    </>
+}
+
+
+
+function FilterableProductTable({products}) {
+    return <>
+        <SearchBar></SearchBar>
+        <ProductTable products={products}></ProductTable>
+    </>
+}
+
+
+export default FilterableProductTable
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FilterableProductTable from "./Product/FilterableProductTable";
 
+
+
+const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <div className="FilterableProductTableContainer"><FilterableProductTable products={PRODUCTS}></FilterableProductTable></div>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
